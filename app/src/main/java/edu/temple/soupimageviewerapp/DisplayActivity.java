71435482218d05b2db2d0e6481293cc5e7a01f19
GrantGwiaz <61506873+GrantGwiaz@users.noreply.github.com>
2021-02-28 22:48:39 -0500

    Receive Intent and display image based on intent extra int "Image"

--- app/src/main/java/edu/temple/soupimageviewerapp/DisplayActivity.java
+++ app/src/main/java/edu/temple/soupimageviewerapp/DisplayActivity.java
@@ -2,13 +2,26 @@ package edu.temple.soupimageviewerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        imageView = findViewById(R.id.imageView);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        int image = extras.getInt("Image");
+
+        imageView.setImageResource(image);
     }
 }
