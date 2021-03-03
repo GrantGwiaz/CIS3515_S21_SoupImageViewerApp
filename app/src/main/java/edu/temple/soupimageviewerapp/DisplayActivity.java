78819d7ae3b2fd0424225df24b0d348c7bdc3c5b
GrantGwiaz <61506873+GrantGwiaz@users.noreply.github.com>
2021-03-03 12:37:37 -0500

    textView text changes to the soup name specified in the intent

--- app/src/main/java/edu/temple/soupimageviewerapp/DisplayActivity.java
+++ app/src/main/java/edu/temple/soupimageviewerapp/DisplayActivity.java
@@ -1,27 +1,33 @@
 package edu.temple.soupimageviewerapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayActivity extends AppCompatActivity {
 
-    ImageView imageView;
+    private ImageView imageView;
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
 
+        textView = findViewById(R.id.textViewSoupName);
         imageView = findViewById(R.id.imageView);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
+
+        String name = extras.getString("Name");
         int image = extras.getInt("Image");
 
+        textView.setText(name);
         imageView.setImageResource(image);
     }
 }
