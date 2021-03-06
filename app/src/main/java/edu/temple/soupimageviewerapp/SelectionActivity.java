5443bfa8c734f7f331c879ca0c4ee15ebc7f70f7
GrantGwiaz <61506873+GrantGwiaz@users.noreply.github.com>
2021-03-06 15:50:41 -0500

    Remove Spinner and grid

--- app/src/main/java/edu/temple/soupimageviewerapp/SelectionActivity.java
+++ app/src/main/java/edu/temple/soupimageviewerapp/SelectionActivity.java
@@ -7,13 +7,10 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.GridView;
+
 
 public class SelectionActivity extends AppCompatActivity {
-
-    private Spinner spinner;
-
 
     private int[] soupImagesArray;
     private String[] soupArray;
@@ -26,34 +23,28 @@ public class SelectionActivity extends AppCompatActivity {
 
         this.setTitle(getResources().getString(R.string.selection_act_name));
         // get our views so we can use them
-        spinner = findViewById(R.id.spinner);
 
         //declare and populate arrays for each for our items, their descriptions, and images of them to be displayed
-        soupArray = new String[]{"Please select a Soup","Gazpacho", "French Onion Soup", "Chowder", "Tomato Soup"};
+        soupArray = new String[]{"Gazpacho", "French Onion Soup", "Chowder", "Tomato Soup"};
         soupImagesArray = new int[]{R.drawable.gazpacho , R.drawable.french_onion , R.drawable.chowder , R.drawable.tomato};
         // all this is better as an object class as to encapsulate, but since won't be reusing or maintaining this its unnecessary
 
-
+        GridView grid = (GridView) findViewById(R.id.GridView);
         ImageAdapter adapter = new ImageAdapter(this, soupArray, soupImagesArray);
-        spinner.setAdapter(adapter);
+        grid.setAdapter(adapter);
 
-        //spinner listener
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        //grid listener
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(position > 0) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                     Intent intent = new Intent(SelectionActivity.this, DisplayActivity.class);
-                    intent.putExtra("Image", soupImagesArray[position-1]);
+                    intent.putExtra("Image", soupImagesArray[position]);
                     intent.putExtra("Name", soupArray[position]);
                     startActivity(intent);
-                    spinner.setSelection(0);
-                }
             }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Toast.makeText(SelectionActivity.this, "Item Unselected", Toast.LENGTH_SHORT).show();
-            }
-
         });
+
     }
+
 }
